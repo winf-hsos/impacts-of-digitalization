19188e2236a6b2f3f5baba9882257ee252274f8b
Nicolas <nicolas.meseth@googlemail.com>
2021-03-12 16:47:19 +0100

    Added d)

---
+++ exercises/1_1_web_app_basics/d/script.js
@@ -0,0 +1,29 @@
+/* global log, getInput */
+log("Solution for exercise 1.1 d)");
+
+function calculate() {
+    // Get the radius from the user
+    var radius = getInput("radius");
+
+    // Make it a number
+    radius = parseFloat(radius);
+
+    log("You entered the following radius: " + radius);
+
+    var area = calculateArea(radius);
+    var circumference = calculateCircumference(radius);
+
+    log("The area is: " + area.toFixed(2));
+    log("The circumference is: " + circumference.toFixed(2));
+
+}
+
+function calculateArea(radius) {
+    return Math.PI * radius * radius;
+
+}
+
+function calculateCircumference(radius) {
+    return 2 * Math.PI * radius;
+
+}
