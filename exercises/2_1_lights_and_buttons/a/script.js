9e346e9c00a0052872d541af5041d1e41cf33a92
Nicolas <nicolas.meseth@googlemail.com>
2021-03-15 15:24:41 +0100

    Updated website

---
+++ exercises/2_1_lights_and_buttons/a/script.js
@@ -0,0 +1,43 @@
+/* global log, getInput, tf */
+log("Solution for exercise 2.1 a) Light up the LED");
+
+// Create a global variable to store the found devices
+var devices;
+var led;
+
+// Initialite connected devices
+tf.initDevices(initDone);
+
+// Call this when all connections are established
+function initDone(connectedDevices) {
+
+    if (connectedDevices.length === 0) {
+        log("Oops, didn't find any devices! Make sure they are connected and refresh this page!");
+    }
+
+    // Store the devices on the global variable
+    devices = connectedDevices;
+
+    // Get the LED light
+    led = devices.getDeviceByIdentifier(271);
+}
+
+function green() {
+    // Set the LED to green
+    led.setColor(0, 255, 0);
+    log("LED should now be green!")
+}
+
+function setColor() {
+    // Get the value from the color's input field
+    var hex = getInput("hexColor");
+
+    // Remove the hashtag if present
+    hex = hex.replace("#", "");
+    var rgb = [(x = parseInt(hex, 16)) >> 16 & 255, x >> 8 & 255, x & 255];
+
+    // Set the color of the LED
+    led.setColor(rgb[0], rgb[1], rgb[2]);
+
+    log("I set the color to R = " + rgb[0] + ", G = " + rgb[1] + ", B = " + rgb[2]);
+}
