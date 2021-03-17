e719a138b9072e2e050a90699d7c30e81dc5c5fc
Nicolas <nicolas.meseth@googlemail.com>
2021-03-18 00:07:37 +0100

    Updated website

---
+++ exercises/1_start_coding/1_1_buttons_and_inputs/a/script.js
@@ -0,0 +1,10 @@
+/* global log, getInput */
+log("Solution for exercise 1.1 a)");
+
+// Log "Hello World" to the console
+log("Hello World!");
+
+function printText() {
+    var text = getInput("myInput");
+    log(text)
+}
